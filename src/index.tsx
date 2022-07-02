3cfbaf7daa4d7bf2c8b46a7c2e28d6a7688bebe3
DongHeon Lee <niche3600@gmail.com>
2022-07-02 10:47:58 +0900

    mock service worker導入
    - 全体的な手順
      - [Install - Getting Started - Mock Service Worker Docs](https://mswjs.io/docs/getting-started/install)
    - 初期化時に `npx msw init public/ --save` をすることで、mock service workerを起動するためのコードを配置する

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,12 @@ import { Provider } from 'react-redux';
 import Navigation from './components/parts/Navigation';
 import PageRoutes from './components/routes';
 
+if (process.env.NODE_ENV === 'development') {
+  console.log('development');
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
+
 const App = () => {
   return (
     <Provider store={store}>
